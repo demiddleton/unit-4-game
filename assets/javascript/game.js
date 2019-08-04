093675bc41bc4c59f04df3c30485a548c49f9952
Denise Middleton <demiddleton@bellsouth.net>
2019-08-04 10:44:42 -0400

    displayed wins, losses and score to the html

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,18 +2,22 @@
 var targetNum = Math.floor(Math.random() * 102 + 19);
 console.log(targetNum);
 
-//Declare variables to track wins and losses
+//Declare variables to track wins, losses and totalScore
 var wins = 0;
 var losses = 0;
+var totalScore = 0;
 
 //Create variables that hold references to places in the HTML file to display the outcome.
 var targetNumText = document.getElementById("targetNum-text");
 var winsText = document.getElementById("wins-text");
 var lossesText = document.getElementById("losses-text");
-var totalScore = document.getElementById("totalScore-text");
+var totalScoreText = document.getElementById("totalScore-text");
 
 
 
 
 //Dispaly the game data on the HTML
 targetNumText.textContent = targetNum;
+winsText.textContent = ("Wins: " + wins);
+lossesText.textContent = ("Losses: " + losses);
+totalScoreText.textContent = totalScore;
