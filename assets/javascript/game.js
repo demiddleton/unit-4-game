2c27b9ca0a6998bb5e20bdf3de803048734bbd58
Denise Middleton <demiddleton@bellsouth.net>
2019-08-04 10:10:10 -0400

    declared variables to track wins, losses, totalScore

---
+++ assets/javascript/game.js
@@ -0,0 +1,12 @@
+//Declare a targetNum array from 19 - 120
+var targetNum = [];
+
+//Declare variables to track wins and losses
+var wins = 0;
+var losses = 0;
+
+//Create variables that hold references to places in the HTML file to display the outcome.
+var targetNumText = document.getElementById("targetNum-text");
+var winsText = document.getElementById("wins-text");
+var lossesText = document.getElementById("losses-text");
+var totalScore = document.getElementById("totalScore-text");
